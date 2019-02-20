faeac9c269a36b8de5b1af4488131b76ccfa1f10
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-20 12:09:44 -0800

    completes recipe_batches.py; runtime O(n), space O(n)

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,29 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    # Loop through keys in recipe
+    # Calculate ingredients[key] // recipe[key] and store result in a batch list
+    # min(list) = max number of batches since max_batch dictated my limiting ingredient
+    # runtime O(n); space O(n)
+    batch = []
+    for ingredient in recipe.keys():
+        if ingredient in ingredients.keys():
+            batch.append(ingredients[ingredient] // recipe[ingredient])
+        else:
+            return 0  # missing necessary ingredient
+    return min(batch)
+
+
+recipe_batches({'milk': 100, 'butter': 50, 'cheese': 10},
+               {'milk': 198, 'butter': 52, 'cheese': 10})
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
