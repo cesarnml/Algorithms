d29aabffc28a30b90a8cbf1dac4405e92c75c8a8
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-02-20 15:05:59 -0800

    completes rock_paper_scissors

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,28 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    options = ['rock', 'paper', 'scissors']
+    outcomes = []
+    if n == 0:
+        return [[]]
+
+    def helper(play, i):
+        for option in options:
+            play = play + [option]
+            if i == n:
+                outcomes.append(play)
+            else:
+                helper(play, i + 1)
+            play = play[:-1]
+    helper([], 1)
+    return outcomes
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
